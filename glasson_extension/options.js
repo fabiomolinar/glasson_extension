717cbcca647ca1f8e09933b21dd0f545533270ab
Fabio Thomaz Molinar <fabiomolinar@gmail.com>
2019-12-03 09:30:05 +0100

    adding salt option

--- glasson_extension/options.js
+++ glasson_extension/options.js
@@ -1,15 +1,42 @@
-let page = document.getElementById('buttonDiv');
-  const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
-  function constructOptions(kButtonColors) {
-    for (let item of kButtonColors) {
-      let button = document.createElement('button');
-      button.style.backgroundColor = item;
-      button.addEventListener('click', function() {
-        chrome.storage.sync.set({color: item}, function() {
-          console.log('color is ' + item);
-        })
-      });
-      page.appendChild(button);
+let saveSalt = document.getElementById("saveSalt");
+let saltInput = document.getElementById("salt");
+let messagesDiv = document.getElementById("messagesDiv");
+let messages = document.getElementById("messages")
+
+function hideMessage(){
+    messages.textContent = "";
+    messagesDiv.style.display = "none";
+    messages.style.display = "none";
+}
+
+function sendMessage(content, level = "info"){
+    messagesDiv.style.display = "block";
+    messages.style.display = "block";
+    messages.textContent = content;
+    setMessageColor(level);
+}
+
+function setMessageColor(level = "info"){
+    switch(level){
+        case "warning":
+            messages.style.color = "red";
+            break;
+        case "success":
+            messages.style.color = "green";
+            break;
+        default:
+            messages.style.color = "black";
     }
-  }
-  constructOptions(kButtonColors);
+}
+
+saveSalt.addEventListener("click", function(){
+    let salt = saltInput.value;
+    if (salt == ""){
+        sendMessage("Wartosc nie moze byc pusta.", "warning");
+        return
+    }
+    chrome.storage.sync.set(
+        {salt: salt},
+        sendMessage("Zapisane", "success")
+    );
+});
